7b97a33aa62592eae0ae05ee50438a8c0eadf8f6
Pankaj-Patil22 <55840069+Pankaj-Patil22@users.noreply.github.com>
2020-02-09 22:35:13 +0530

    final expt 2

--- 2.c
+++ 2.c
@@ -1,19 +1,32 @@
 #include<stdio.h>
 
+int c=0;
+
 float merge(int l,int m,int r,float a[])
 {
     int i,j,k;
     int n1=m-l+1,n2=r-m;
     float L[n1],R[n2];
     for(i=0;i<n1;i++)
-    L[i]=a[l+i];
+    {
+        c++;
+        L[i]=a[l+i];
+    }
+    c++;
+
     for(i=0;i<n2;i++)
-    R[i]=a[m+1+i];
+    {
+        c++;
+        R[i]=a[m+1+i];
+    }
+    c++;
 
     i=0,j=0;
 
     while(i<n1 && j<n2)
     {
+        c++;
+        c++;
         if(L[i]<=R[j]) 
         { 
             a[k]=L[i]; 
@@ -24,22 +37,28 @@ float merge(int l,int m,int r,float a[])
             a[k]=R[j]; 
             j++; 
         } 
+        c++;
         k++; 
     }
+    c=c+2;
 
     while(i<n1) 
     { 
+        c++;
         a[k]=L[i]; 
         i++; 
         k++; 
     } 
+    c++;
 
     while(j<n2) 
     { 
+        c++;
         a[k]=R[j]; 
         j++; 
         k++; 
     }
+    c++;
 
 
 }
@@ -49,12 +68,14 @@ float mergesort(int l, int r,float a[])
     int m=(l+r)/2;
     if(l<r)
     {   
+        c++;
         //l-left,r-right
         mergesort(l,m,a);
         mergesort(m+1,r,a);
 
         merge(l,m,r,a);
     }
+    c++;
 }
 
 int main() 
@@ -75,6 +96,8 @@ int main()
     printf("The sorted array is\n");
     for(i=0;i<n;i++)
     printf("%f ",a[i]);
+
+    printf("\nThe no of comparisions made are %d",c);
 
     return 0; 
 } 
